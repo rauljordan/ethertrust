21b24fb9539d5a8e56630bc9619737a7f4a47817
Raul Jordan <raulitojordan@gmail.com>
2017-09-22 13:25:12 -0500

    add babel

--- scrapers/etherscan.js
+++ scrapers/etherscan.js
@@ -2,5 +2,6 @@ import cheerio from 'cheerio';
 import rp from 'request-promise';
 
 export default async function etherscan() {
-  
+  const res = await rp('https://etherscan.io');
+  console.log(res);
 }
